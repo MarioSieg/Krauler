cf491c8f8f6acf210fcb073e7dc847233fb8fbe7
msi <support@kerbogames.com>
2021-04-19 15:45:03 +0200

    Initial

---
+++ Krauler/ServerHeader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Krauler
+{
+    /// <summary>
+    /// Contains information about the crawl address.
+    /// </summary>
+    public struct ServerHeader
+    {
+        public Uri Uri;
+        public bool Locked;
+
+        public int Port => Uri.Port;
+        public string Host => Uri.Host;
+        public string AbsolutePath => Uri.AbsolutePath;
+        public string AbsoluteUri => Uri.AbsoluteUri;
+        public string PathAndQuery => Uri.PathAndQuery;
+        public string Query => Uri.Query;
+
+        public static readonly ServerHeader DefaultTargetNoProxy = new()
+        {
+            Uri = new Uri("localhost:8080"),
+            Locked = false
+        };
+}
+}
---
+++ Krauler/TestCrawler.cs
@@ -0,0 +1,26 @@
+﻿namespace Krauler
+{
+    class TestCrawler : ICrawler
+    {
+        public string Name => "Test Crawler";
+
+        public string Description => "Test Crawler";
+
+        public ServerHeader ServerHeader => ServerHeader.DefaultTargetNoProxy;
+
+        public void OnCreate()
+        {
+            Logger.Instance.WriteLine($"Created Crawler with Name: {Name}");
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void OnShutdown()
+        {
+
+        }
+    }
+}
