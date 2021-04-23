8ea80ebc12e2b41b11f043a0e74749bc91495161
msi <support@kerbogames.com>
2021-04-23 21:46:23 +0200

    Update GoogleCrawler.cs

--- Krauler/Crawlers/GoogleCrawler.cs
+++ Krauler/Crawlers/GoogleCrawler.cs
@@ -4,22 +4,13 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.DevTools.V85.IndexedDB;
-using OpenQA.Selenium.Support.UI;
 
 namespace Krauler.Crawlers
 {
     public sealed class GoogleCrawlerConfig : BaseConfig
     {
-        public bool ChromeDriverHideCommandPromptWindow = true;
-        
-        public bool UseProxy = false;
-
-        public readonly bool SetUserAgent = false;
-        
         public readonly List<string> DefaultChromeOptions = new()
         {
             //"--window-size=800,600",
@@ -35,9 +26,17 @@ namespace Krauler.Crawlers
             "--silent"
         };
 
+        public readonly bool SetUserAgent = false;
+        public bool ChromeDriverHideCommandPromptWindow = true;
+
         public PageLoadStrategy PageLoadStrategy = PageLoadStrategy.Normal;
 
-        public byte TabCount = 2;
+        public nuint TabCount = 2;
+
+        // -1 = loop forever
+        public nint RetryCount = -1;
+
+        public bool UseProxy = false;
 
         public GoogleCrawlerConfig() : base("http://google.com") { }
     }
@@ -46,6 +45,9 @@ namespace Krauler.Crawlers
     {
         private readonly GoogleCrawlerConfig _config;
         private IWebDriver? _driver;
+
+        private readonly Regex linkParser =
+            new(@"\b(?:https?://|www\.)\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         public GoogleCrawler() : base("GoogleCrawler", "")
         {
@@ -56,24 +58,26 @@ namespace Krauler.Crawlers
         {
             var service = ChromeDriverService.CreateDefaultService();
             service.HideCommandPromptWindow = _config.ChromeDriverHideCommandPromptWindow;
-            
+
             var options = new ChromeOptions {PageLoadStrategy = _config.PageLoadStrategy};
             options.AddArguments(_config.DefaultChromeOptions);
-            
+
             if (_config.UseProxy)
             {
                 var rand = new Random();
                 var proxy = CrawlerFactory.Proxies.Value[rand.Next(CrawlerFactory.Proxies.Value.Length)];
                 options.AddArgument("--proxy-server=http://" + proxy);
             }
+
             if (_config.SetUserAgent)
             {
                 var rand = new Random();
                 var userAgent = CrawlerFactory.UserAgents.Value[rand.Next(CrawlerFactory.UserAgents.Value.Length)];
                 options.AddArgument("--user-agent=" + userAgent);
             }
+
             _driver = new ChromeDriver(service, options);
-            
+
             _driver.Manage().Cookies.DeleteAllCookies();
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
@@ -92,62 +96,52 @@ namespace Krauler.Crawlers
             // tbm=isch tells it you want to search Google Images. It stands for to be matched = image search.
             // ved decodes to tell Google what links you clicked on previous pages on Google to get to the current page (https://valentin.app/ved.html)
             // oq = The original query you wanted to search for that you typed in (see q)
-            
-            
+
+
             Debug.Assert(_driver != null, nameof(_driver) + " != null");
             const string? query = "KevinKlang";
-            
+
 
             var confirm = true; // Google confirm usage needs to be done only at first call
             var resultList2 = new List<Match>();
-            
+
             for (uint i = 1; i < 8; ++i)
             {
-                _driver.Navigate().GoToUrl($"{_config.ServerHeader.Uri}/search?q={query}&start={(i-1)*10}");
-                
+                _driver.Navigate().GoToUrl($"{_config.ServerHeader.Uri}/search?q={query}&start={(i - 1) * 10}");
+
                 if (i > 1) confirm = false;
-                
+
                 if (_driver.FindElementSafe(By.TagName("button")) != null && confirm)
-                {
                     GoogleUsageConfirmer(By.TagName("button"));
-                }
                 if (_driver.FindElementSafe(By.Id("zV9nZe")) != null && confirm)
-                {
                     GoogleUsageConfirmer(By.XPath("//button[@id='zV9nZe']"));
-                }
-            
+
                 //IWebElement element = _driver.FindElement(By.XPath("//input[@name='q']"));
-            
+
                 //element.SendKeys("Kevin Klang");
                 //element.SendKeys(Keys.Enter);
 
                 IWebElement resultsPanel = _driver.FindElement(By.Id("search"));
-                
+
                 ReadOnlyCollection<IWebElement> searchResults = resultsPanel.FindElements(By.XPath(".//a"));
                 SubmitData(searchResults.Select(x => x.Text));
             }
-            
         }
-        
-        private Regex linkParser = new Regex(@"\b(?:https?://|www\.)\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-        
+
         protected override IEnumerable<string> DataProcessor(IEnumerable<string> rawText)
         {
             foreach (var text in rawText)
-            {
                 if (text.Contains("http"))
                     yield return linkParser.Match(text).Value;
-            }
             DumpResults();
         }
 
         private void GoogleUsageConfirmer(By tagName)
         {
             uint i = 0;
-            if (_driver?.WindowHandles == null) 
+            if (_driver?.WindowHandles == null)
                 throw new NullReferenceException();
             foreach (var handle in _driver.WindowHandles)
-            {
                 try
                 {
                     _driver.SwitchTo().Window(handle);
@@ -158,7 +152,6 @@ namespace Krauler.Crawlers
                 {
                     Logger.Instance.Write(ex);
                 }
-            }
         }
 
         public override void OnDestroy()
